e660c63f6de83029343fade3741f23691012a452
Davis Weimer <davisweimer@gmail.com>
2023-11-30 14:12:03 -0700

    feat: Mobile SideNav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import SideNav from './components/SideNav'
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <main>
+      <SideNav />
+    </main>
   )
 }
 
