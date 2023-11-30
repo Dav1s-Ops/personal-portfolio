cb8542577e3f9635fd85e5bc2927fce00c4d2ab2
Davis Weimer <davisweimer@gmail.com>
2023-11-30 14:28:23 -0700

    feat: SideNav Desktop

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -22,11 +22,11 @@ const SideNav = () => {
               <AiOutlineHome size={20} />
               <span className='pl-4'>Home</span>
             </a>
-            <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-105 ease-in duration-200'>
+            <a href='#work' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-105 ease-in duration-200'>
               <GrProjects size={20} />
               <span className='pl-4'>Work</span>
             </a>
-            <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-105 ease-in duration-200'>
+            <a href='#projects' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-105 ease-in duration-200'>
               <AiOutlineProject size={20} />
               <span className='pl-4'>Projects</span>
             </a>
@@ -34,19 +34,35 @@ const SideNav = () => {
               <BsPerson size={20} />
               <span className='pl-4'>Resume</span>
             </a>
-            <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-105 ease-in duration-200'>
+            <a href='#contact' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-105 ease-in duration-200'>
               <AiOutlineMail size={20} />
               <span className='pl-4'>Contact</span>
             </a>
           </div>
         ) : (
-          <div>
-
+          <div className='md:block hidden fixed top-[25%] z-10'>
+            <div className='flex flex-col'>
+              <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-5 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                <AiOutlineHome size={25} />
+              </a>
+              <a href='#work' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-5 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                <GrProjects size={25} />
+              </a>
+              <a href='#projects' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-5 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                <AiOutlineProject size={25} />
+              </a>
+              <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-5 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                <BsPerson size={25} />
+              </a>
+              <a href='#contact' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-5 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                <AiOutlineMail size={25} />
+              </a>
+            </div>
           </div>
         )
       }
       <div>
-        
+
       </div>
     </div>
   );
