12d6347ba2b9a9659d3c977a7d897fbfadef0a6b
Davis Weimer <davisweimer@gmail.com>
2023-11-30 17:00:29 -0700

    feat: Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import SideNav from './components/SideNav'
 import Main from './components/Main'
 import Work from './components/Work'
+import Projects from './components/Projects'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <SideNav />
       <Main />
       <Work />
+      <Projects />
     </main>
   )
 }
