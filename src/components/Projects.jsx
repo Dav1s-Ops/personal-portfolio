93cc42046a7e75f9127ca67c2e9a0372155eafbe
Davis Weimer <davisweimer@gmail.com>
2023-12-04 18:31:48 -0700

    refac: Updated project info

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,30 +1,40 @@
 import React from 'react'
 import ProjectItem from './ProjectItem'
-import codingImg from '../assets/coding.avif'
+import RefugeeAidImg from '../assets/RefugeeAid.png'
+import LabelLensImg from '../assets/LabelLens.png'
+import CodingImg from '../assets/coding.avif'
 
 const Projects = () => {
   return (
     <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
       <h1 className='text-4xl font-mussels-bold text-center text-dark_gray'>Projects</h1>
       <p className='font-mussels text-center py-8'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Here are some personal and group projects I'd like to highlight. Whether I'm working solo or collaborating, coding is one of my favorite passions. I'm always pushing myself to learn challenging new concepts and technologies. If you'd like to work on a project with me or are interested to learn more about a project below, don't hesitate to reach out through the contact form below!
       </p>
       <div className='grid sm:grid-cols-2 gap-12'>
         <ProjectItem 
-          img={codingImg}
-          title='Placeholder Project'
+          img={RefugeeAidImg}
+          title='Refugee Aid'
+          framework='React JS'
+          link='https://refugee-aid-fe.vercel.app/'
         />
         <ProjectItem 
-          img={codingImg}
-          title='Placeholder Project'
-        />
+          img={LabelLensImg}
+          title='Label Lens'
+          framework='Ruby on Rails'
+          link={'https://label-lens.onrender.com/'}
+          />
         <ProjectItem 
-          img={codingImg}
-          title='Placeholder Project'
-        />
+          img={CodingImg}
+          title='Sweater Whether API'
+          framework='Ruby on Rails'
+          link={'https://github.com/DavisWeimer/whether_sweater_api'}
+          />
         <ProjectItem 
-          img={codingImg}
-          title='Placeholder Project'
+          img={CodingImg}
+          title='Simple MB Compressor'
+          framework='C++'
+          link={'https://github.com/DavisWeimer/SimpleMBComp'}
         />
       </div>
     </div>
