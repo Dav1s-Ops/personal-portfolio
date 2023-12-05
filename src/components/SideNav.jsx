c61ac0d76ec83d4cb762efea25e983a3ced2cd6a
Davis Weimer <davisweimer@gmail.com>
2023-12-05 11:55:44 -0700

    feat: SideNav reveal name on hover

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -45,21 +45,36 @@ const SideNav = () => {
         ) : (
           <div className='md:block hidden fixed top-[25%] z-10 '>
             <div className='flex flex-col'>
-              <a href='#main'  className='rounded-full shadow bg-dark_gray shadow-gray-500 hover:bg-gray-500 m-5 p-4 cursor-pointer hover:scale-110 ease-in duration-200 '>
-                <AiOutlineHome alt='Home' style={{color: '#A4F236'}} size={25} />
-              </a>
-              <a href='#work' className='rounded-full shadow bg-dark_gray shadow-gray-500 hover:bg-gray-500 m-5 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-                <GrProjects style={{color: '#A4F236'}} size={25} />
-              </a>
-              <a href='#projects' className='rounded-full shadow bg-dark_gray shadow-gray-500 hover:bg-gray-500 m-5 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-                <AiOutlineProject style={{color: '#A4F236'}} size={25} />
-              </a>
-              <a href='#about-me' className='rounded-full shadow bg-dark_gray shadow-gray-500 hover:bg-gray-500 m-5 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-                <BsPerson style={{color: '#A4F236'}} size={25} />
-              </a>
-              <a href='#contact' className='rounded-full shadow bg-dark_gray shadow-gray-500 hover:bg-gray-500 m-5 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-                <AiOutlineMail style={{color: '#A4F236'}} size={25} />
-              </a>
+            <a href='#main' className='group relative flex items-center justify-center rounded-full bg-dark_gray hover:scale-110 m-5 p-4 cursor-pointer transition-all ease-in duration-300 w-14 h-14'>
+              <div className='inline-flex justify-center items-center '>
+                <AiOutlineHome className='text-alien_green z-20' size={25} />
+                <span className='absolute left-7 w-0 overflow-hidden bg-dark_gray rounded-r-full transition-all ease-in duration-200 group-hover:w-32 h-full flex items-center justify-center text-alien_green font-mussels text-opacity-0 group-hover:text-opacity-100'>Home</span>
+              </div>
+            </a>
+            <a href='#work' className='group relative flex items-center justify-center rounded-full shadow bg-dark_gray hover:scale-110 m-5 p-4 cursor-pointer transition-all ease-in duration-200 w-14 h-14'>
+              <div className='inline-flex justify-center items-center '>
+                <GrProjects className='text-alien_green z-20' size={25} />
+                <span className='absolute left-7 w-0 overflow-hidden bg-dark_gray rounded-r-full transition-all ease-in duration-200 group-hover:w-32 h-full flex items-center justify-center text-alien_green font-mussels text-opacity-0 group-hover:text-opacity-100'>Work</span>
+              </div>
+            </a>
+            <a href='#projects' className='group relative flex items-center justify-center rounded-full shadow bg-dark_gray hover:scale-110 m-5 p-4 cursor-pointer transition-all ease-in duration-200 w-14 h-14'>
+              <div className='inline-flex justify-center items-center '>
+                <AiOutlineProject className='text-alien_green z-20' size={25} />
+                <span className='absolute left-7 w-0 overflow-hidden bg-dark_gray rounded-r-full transition-all ease-in duration-200 group-hover:w-32 h-full flex items-center justify-center text-alien_green font-mussels text-opacity-0 group-hover:text-opacity-100'>Projects</span>
+              </div>
+            </a>
+            <a href='#about-me' className='group relative flex items-center justify-center rounded-full shadow bg-dark_gray hover:scale-110 m-5 p-4 cursor-pointer transition-all ease-in duration-200 w-14 h-14'>
+              <div className='inline-flex justify-center items-center '>
+                <BsPerson className='text-alien_green z-20' size={25} />
+                <span className='absolute left-7 w-0 overflow-hidden bg-dark_gray rounded-r-full transition-all ease-in duration-200 group-hover:w-32 h-full flex items-center justify-center text-alien_green font-mussels text-opacity-0 group-hover:text-opacity-100'>About</span>
+              </div>
+            </a>
+            <a href='#contact' className='group relative flex items-center justify-center rounded-full shadow bg-dark_gray hover:scale-110 m-5 p-4 cursor-pointer transition-all ease-in duration-200 w-14 h-14'>
+              <div className='inline-flex justify-center items-center '>
+                <AiOutlineMail className='text-alien_green z-20' size={25} />
+                <span className='absolute left-7 w-0 overflow-hidden bg-dark_gray rounded-r-full transition-all ease-in duration-200 group-hover:w-32 h-full flex items-center justify-center text-alien_green font-mussels text-opacity-0 group-hover:text-opacity-100'>Contact</span>
+              </div>
+            </a>
             </div>
           </div>
         )
