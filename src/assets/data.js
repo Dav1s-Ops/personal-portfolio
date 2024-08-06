02b0a70eaa3e06b52a08e58b4b9759d33b1346f5
Davis Weimer <davisweimer@gmail.com>
2024-08-06 10:52:29 -0600

    Add new gig!

--- src/assets/data.js
+++ src/assets/data.js
@@ -2,12 +2,22 @@ export const data = [
   {
     year: 2024,
     title: 'Software Engineer',
-    company: 'Tack Mobile',
-    website: 'https://tackmobile.com/',
-    duration: '3 months',
+    company: 'ThxRex',
+    website: 'https://thxrex.com/',
+    duration: 'Current',
     inSchool: false,
     details: 'Worked on the Accolader web-based employee recognition system. I implemented new planned features. In addition, I improved the test coverage in the codebase to improve the code quality.',
-    list: ['Languages: HTML5, CSS3, Ruby, JavaScript, C++', 'Frameworks/Technology: Rails, React PostgreSQL, GraphQL, Node.js, Bootstrap, Vite, Tailwind', 'Development Tools: VSCode, Git, Postman']
+    list: 'https://skillicons.dev/icons?i=ruby,rails,js,tailwind,heroku,docker'
+  },
+  {
+    year: 2024,
+    title: 'Software Engineer',
+    company: 'Tack Mobile',
+    website: 'https://tackmobile.com/',
+    duration: '4 months',
+    inSchool: false,
+    details: 'Worked on the Accolader web-based employee recognition system. I implemented new planned features. In addition, I improved the test coverage in the codebase to improve the code quality.',
+    list: 'https://skillicons.dev/icons?i=ruby,rails,js,redis,aws,docker'
   },
   {
     year: 2023,
@@ -16,8 +26,7 @@ export const data = [
     website: 'https://turing.edu/',
     duration: '9 months',
     inSchool: true,
-    details: 'Began my journey into Software Engineering after completing the Backend Engineering program at Turing School of Software & Design.',
-    list: ['Languages: HTML5, CSS3, Ruby, JavaScript, C++', 'Frameworks/Technology: Rails, React PostgreSQL, GraphQL, Node.js, Bootstrap, Vite, Tailwind', 'Development Tools: VSCode, Git, Postman']
+    details: 'Began my journey into Software Engineering after completing the Backend Engineering program at Turing School of Software & Design.'
   },
   {
     year: 2022,
@@ -26,8 +35,7 @@ export const data = [
     website: 'https://brownnote.com/integration/services/',
     duration: '1 year',
     inSchool: false,
-    details: 'I returned to Brown Note Productions and joined the Integration Department. My role involved a variety of responsibilities including networking, AVL shipping and receiving, and AVL rack building and design. My primary focus was on optimizing these designs and hardware environments. Additionally, I oversaw on-site deployment, successfully managing hardware and software installations.',
-    list: ['Networking: Designed efficient communication solutions.', 'AVL Shipping/Receiving: Ensured timely component delivery for project deadlines.', 'AVL Rack Building/Design: Optimized software hardware environments.', 'On-Site Deployment: Managed successful hardware and software installations.']
+    details: 'I returned to Brown Note Productions and joined the Integration Department, where I focused on networking, AVL shipping and receiving, and AVL rack building and design.'
   },
   {
     year: 2021,
@@ -36,8 +44,7 @@ export const data = [
     website: 'https://www.evergroove.com/',
     duration: '2 years',
     inSchool: false,
-    details: 'I had the opportunity to work with Brad Smalling at his Recording Studio in Evergreen, CO. During my time there, I played a key role in upgrading the studio to a comprehensive Audio over IP (AoIP) Dante Atmos system. My responsibilities also included ensuring high-quality recording sessions in Pro Tools. I enjoyed connecting with the artists and actively giving and recieving feedback during the mixing/recording process. My experience extended to working with different audio codecs, including both stereo and advanced formats like Dolby Atmos (7.1.4).',
-    list: ['Pro Tools: Ensured high-quality recording sessions, leading to successful project executions.', 'Feedback: Enhanced project outcomes by soliciting and incorporating client feedback during mixing.', 'Audio Technology: Experience with different Audio Codecs, including stereo and Dolby Atmos (7.1.4)']
+    details: "I assisted with upgrading the studio to an Audio over IP Dante Atmos system, ensured high-quality Pro Tools recording sessions. For sessions, I connected with artists for feedback and worked with Dolby Atmos (7.1.4)."
   },
   {
     year: 2020,
@@ -46,8 +53,7 @@ export const data = [
     title: 'FOH Engineer',
     duration: '2 years',
     inSchool: false,
-    details: "During the pandemic, I collaborated with Itchy-O to organize and run audio for 'Drive-in' Sypherlot performances around Denver. This unique project also involved recording, mixing, and mastering the Sypherlot shows. My role was crucial in RF coordination, where I used WB6 to scan, deploy, and monitor over 20 channels of audio RF. Additionally, I handled the soundcheck and mixed the shows, as well as managed monitor mixes for the musicians, which was always a blast since the shows were wildy unpredictable at times!",
-    list: ['RF Coordination: Used WB6 to scan, deploy and monitor 20+ channels of audio RF.', 'Mixing: Handle soundcheck and mixed shows as well as monitor mixes for musicians.'] 
+    details: "During the pandemic, I collaborated with Itchy-O to organize and run audio for 'Drive-in' Sypherlot performances in Denver, where I assited with the recording, mixing, and mastering of the shows. I also helped manage RF coordination/deployment for over 20 channels, and handled soundchecks and monitor mixes for musicians amidst wildly unpredictable (but exteremetly exciting) performances." 
   },
   {
     year: 2018,
@@ -56,8 +62,7 @@ export const data = [
     title: 'A2 - Audio Technician',
     duration: '2 years',
     inSchool: false,
-    details: "I specialized in Audio and had the opportunity to work at both local and national festivals and shows, including notable events like the X Games, Global Dance, and Chicago Open Air. My expertise was utilized in various areas, starting with repairing and testing speakers under tight deadlines. I also built professional audio packages, tailored specifically to each client, incorporating equipment preferences when available. On site, I flew line-array systems and helped time align the system before the shows.",
-    list: ['Repaired speakers: Enhanced sound quality by resolving audio issues in speakers.', 'Built comprehensive Audio Packages: Increased client satisfaction through tailored Audio Packages.', 'Loaded / unloaded trucks: Improved setup times and productivity in equipment logistics.', 'Flew Line-array systems: Optimized audience audio experience with PA system positioning.'] 
+    details: "I specialized in audio for local and national festivals, including the X Games, Global Dance, and Chicago Open Air, where I repaired and tested speakers, built tailored audio packages, flew line-array systems, and helped time align the systems before shows."
   },
   {
     year: 2016,
@@ -66,7 +71,6 @@ export const data = [
     title: 'Audio Engineer',
     duration: '4 years',
     inSchool: true,
-    details: "Completed my BA of Science in Recording Arts.",
-    list: ['Repaired speakers: Enhanced sound quality by resolving audio issues in speakers.', 'Built comprehensive Audio Packages: Increased client satisfaction through tailored Audio Packages.', 'Loaded / unloaded trucks: Improved setup times and productivity in equipment logistics.', 'Flew Line-array systems: Optimized audience audio experience with PA system positioning.'] 
+    details: "Completed my BA of Science in Recording Arts." 
   }
 ]
