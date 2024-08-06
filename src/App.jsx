421752d2cfd47c67102e4fd70b0a84103edde2ea
Davis Weimer <davisweimer@gmail.com>
2024-08-06 16:48:45 -0600

    Adjust Sound Enabled? modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { useState, useRef, useEffect } from 'react'
-import SideNav from './components/SideNav'
-import Main from './components/Main'
-import Work from './components/Work'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import AboutMe from './components/AboutMe'
-import HoverSound from './assets/sounds/vk_Focus.wav'
-import ClickSound from './assets/sounds/vk_Select.wav'
-import NotifySound from './assets/sounds/NotifyPopup.wav'
-import BladeOpenSound from './assets/sounds/BladeOpen.wav'
-import BladeSwitch1 from './assets/sounds/BladeSwitch_1.wav'
-import BladeSwitch2 from './assets/sounds/BladeSwitch_2.wav'
-import BladeSwitch3 from './assets/sounds/BladeSwitch_3.wav'
-import BlipHoverSound from './assets/sounds/snd_channelup.wav'
-import BlipClickSound from './assets/sounds/snd_channeldown.wav'
-import NavHoverSound from './assets/sounds/btn_backG.wav'
-import TimelineSound from './assets/sounds/tab_Switch.wav'
+import { useState, useRef, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { TiTick } from "react-icons/ti";
+import { TiTimes } from "react-icons/ti";
+import SideNav from './components/SideNav';
+import Main from './components/Main';
+import Work from './components/Work';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import AboutMe from './components/AboutMe';
+import HoverSound from './assets/sounds/vk_Focus.wav';
+import ClickSound from './assets/sounds/vk_Select.wav';
+import NotifySound from './assets/sounds/NotifyPopup.wav';
+import BladeOpenSound from './assets/sounds/BladeOpen.wav';
+import BladeSwitch1 from './assets/sounds/BladeSwitch_1.wav';
+import BladeSwitch2 from './assets/sounds/BladeSwitch_2.wav';
+import BladeSwitch3 from './assets/sounds/BladeSwitch_3.wav';
+import BlipHoverSound from './assets/sounds/snd_channelup.wav';
+import BlipClickSound from './assets/sounds/snd_channeldown.wav';
+import NavHoverSound from './assets/sounds/btn_backG.wav';
+import TimelineSound from './assets/sounds/tab_Switch.wav';
 
 function App() {
   const hoverSound = useRef(new Audio(HoverSound));
@@ -32,17 +35,17 @@ function App() {
   const [soundEnabled, setSoundEnabled] = useState(null); // null means user has not made a choice yet
 
   // Set volume for each sound
-  hoverSound.current.volume = 0.25;
-  clickSound.current.volume = 0.25;
-  notifySound.current.volume = 0.25;
+  hoverSound.current.volume = 0.2;
+  clickSound.current.volume = 0.2;
+  notifySound.current.volume = 0.2;
   bladeOpenSound.current.volume = 0.2;
   bladeSwitch1.current.volume = 0.2;
   bladeSwitch2.current.volume = 0.2;
   bladeSwitch3.current.volume = 0.2;
-  blipHoverSound.current.volume = 0.25;
-  blipClickSound.current.volume = 0.25;
-  navHoverSound.current.volume = 0.25;
-  timelineSound.current.volume = 0.25;
+  blipHoverSound.current.volume = 0.2;
+  blipClickSound.current.volume = 0.2;
+  navHoverSound.current.volume = 0.2;
+  timelineSound.current.volume = 0.2;
 
   const playSound = (sound) => {
     if (sound) {
@@ -63,25 +66,33 @@ function App() {
 
   return (
     <main>
-      {soundEnabled === null && (
-        <div className="font-mussels fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
-          <div className="bg-gradient-to-t to-dark_gray from-alien_green shadow-lg shadow-gray-400 text-white border-solid border-alien_green border-2 p-6 w-60 flex flex-col justify-center items-center gap-3 rounded-lg text-center">
-            <h2 className="text-lg mb-4">Enable sound?</h2>
-            <button 
-              className="w-[100%] flex justify-center items-center rounded-full shadow-lg bg-dark_gray text-alien_green m-2 p-2 cursor-pointer hover:scale-105 ease-in duration-200" 
-              onClick={() => handleUserChoice(true)}
-            >
-              Yes
-            </button>
-            <button 
-              className="w-[100%] flex justify-center items-center rounded-full shadow-lg bg-dark_gray text-alien_green m-2 p-2 cursor-pointer hover:scale-105 ease-in duration-200" 
-              onClick={() => handleUserChoice(false)}
-            >
-              No
-            </button>
-          </div>
-        </div>
-      )}
+      <AnimatePresence>
+        {soundEnabled === null && (
+          <motion.div 
+            className="fixed w-full h-screen flex flex-col justify-center items-center z-20"
+            initial={{ y: -400, opacity: 0 }}
+            animate={{ y: -270, opacity: 1 }}
+            exit={{ y: -400, opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="bg-gradient-to-b from-alien_green shadow-lg shadow-gray-400 text-dark_gray border-solid border-alien_green border-2 p-6 w-80 h-16 rounded-full flex flex-row justify-center items-center gap-3 text-center">
+              <h2 className="font-mussels-bold text-md">Enable sound? 🔊</h2>
+              <button 
+                className="text-xl font-mussels w-12 flex justify-center items-center rounded-lg shadow-lg bg-dark_gray text-alien_green p-2 cursor-pointer hover:scale-105 ease-in duration-200" 
+                onClick={() => handleUserChoice(true)}
+              >
+                <TiTick />
+              </button>
+              <button 
+                className="text-xl font-mussels w-12 flex justify-center items-center rounded-lg shadow-lg bg-dark_gray text-alien_green p-2 cursor-pointer hover:scale-105 ease-in duration-200" 
+                onClick={() => handleUserChoice(false)}
+              >
+                <TiTimes />
+              </button>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <SideNav 
         soundEnabled={soundEnabled} 
         playSound={playSound} 
@@ -119,4 +130,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
