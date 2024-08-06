0bed7eabcb8d895fdc97f928679f9bf4cbddde83
Davis Weimer <davisweimer@gmail.com>
2024-08-06 16:49:02 -0600

    Refactor styling on About Me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,15 +8,13 @@ const AboutMe = () => {
       <h1 className='py-4 text-4xl font-mussels-bold text-center text-dark_gray'>
         About Me
       </h1>
-      <div className='font-mussels text-center'>
-        <p className='mb-4'>
+      <div className='font-mussels text-center mb-4 flex flex-col lg:flex-row items-center justify-between gap-4'>
+        <img className='w-36 h-36 object-cover rounded-full shadow-md shadow-gray-400' src={MeImg} />
+        <p className='w-3/4'>
         My name is Davis and I'm a Software Engineer with a background in Audio Engineering. When I am not programming I love listening to, collecting and making music. Check out my <a className='hover:underline hover:text-alien_green' href='https://soundcloud.com/berrik' target="_blank" rel="noopener noreferrer">SoundCloud</a> to see what I've been working on. I also enjoy reading (especially sci-fi space operas), playing video games, <a className='hover:underline hover:text-alien_green' href='https://www.mixcloud.com/da-5k/' target="_blank" rel="noopener noreferrer">DJing</a>, and spending time with my partner Mary and our cat.
         </p>
       </div>
-        <div className='flex flex-col items-center'>
-        <img className='mt-2 w-48 h-48 m-6 object-cover rounded-full shadow-lg shadow-gray-400' src={MeImg} />
-        </div>
-      <div className='flex lg:flex-row flex-col gap-2 items-center justify-center'>
+      <div className='flex lg:flex-row flex-col gap-2 items-center justify-between'>
         <div className='bg-dark_gray rounded-lg p-4 shadow-xl shadow-gray-400' style={{ width: '320px', height: '445px' }}>
           <a href="https://open.spotify.com/user/davis.weimer?si=60942ca615b740b6" target="_blank" rel="noopener noreferrer">
             <img className='object-cover w-full h-full' src="https://spotify-github-profile.kittinanx.com/api/view?uid=davis.weimer&cover_image=true&theme=default&show_offline=false&background_color=303030&interchange=false&bar_color=a4f236" alt="spotify-github-profile" />
@@ -34,5 +32,3 @@ const AboutMe = () => {
 };
 
 export default AboutMe;
-
-// 320 x 445
