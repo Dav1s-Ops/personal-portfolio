830723788f07b9a511f4cfe4fec57aafa7cd540b
Davis Weimer <davisweimer@gmail.com>
2024-09-10 10:05:44 -0600

    Adjust Project img to object-cover

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProjectItem = ({img, title, link, belink, felink, framework, soundEnabled, playSound, blipHoverSound, blipClickSound}) => {
   return (
     <div className='font-mussels relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-t from-alien_green to-dark_gray hover:outline hover:outline-2 hover:outline-offset-1 hover:outline-alien_green'>
-      <img src={img} alt={title} className='m-auto h-full w-full rounded-xl group-hover:opacity-10'/>
+      <img src={img} alt={title} className='m-auto h-[300px] w-full object-cover rounded-xl group-hover:opacity-10'/>
       <div className='hidden w-7/12 group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl font-mussels-bold text-white tracking-wider text-center'>
           {title}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,6 @@
-import React from 'react'
-import ProjectItem from './ProjectItem'
-import RefugeeAidImg from '../assets/RefugeeAid.png'
-import LabelLensImg from '../assets/LabelLens.png'
-import WhetherSweater from '../assets/whether-sweater.png'
-import GenreFi from '../assets/genre-fi.png'
+import React from 'react';
+import ProjectItem from './ProjectItem';
+import projectList from '../assets/project-data';
 
 const Projects = ({ soundEnabled, playSound, blipHoverSound, blipClickSound }) => {
   return (
@@ -13,54 +10,24 @@ const Projects = ({ soundEnabled, playSound, blipHoverSound, blipClickSound }) =
         Here are some personal and group projects I'd like to highlight. Whether I'm working solo or collaborating, coding is one of my favorite passions. I'm always pushing myself to learn challenging new concepts and technologies. If you'd like to work on a project with me or are interested to learn more about a featured project, don't hesitate to reach out through the contact form below!
       </p>
       <div className='grid sm:grid-cols-2 gap-12'>
-        <ProjectItem 
-          img={WhetherSweater}
-          title='Whether Sweater App'
-          framework='React + Vite / / Ruby on Rails'
-          belink={'https://github.com/DavisWeimer/whether_sweater_api'}
-          felink={'https://github.com/DavisWeimer/whether-sweater-fe'}
-          link={'https://whether-sweater-fe.vercel.app/'}
-          soundEnabled={soundEnabled} 
-          playSound={playSound} 
-          blipHoverSound={blipHoverSound} 
-          blipClickSound={blipClickSound}
+        {projectList.map((project, index) => (
+          <ProjectItem
+            key={index}
+            img={project.img}
+            title={project.title}
+            framework={project.framework}
+            belink={project.belink}
+            felink={project.felink}
+            link={project.link}
+            soundEnabled={soundEnabled}
+            playSound={playSound}
+            blipHoverSound={blipHoverSound}
+            blipClickSound={blipClickSound}
           />
-        <ProjectItem 
-          img={GenreFi}
-          title='Genre-Fi'
-          framework='React'
-          link={'https://genre-fi.vercel.app/'}
-          felink={'https://github.com/DavisWeimer/genre-fi'}
-          soundEnabled={soundEnabled} 
-          playSound={playSound} 
-          blipHoverSound={blipHoverSound} 
-          blipClickSound={blipClickSound}
-        />
-        <ProjectItem 
-          img={RefugeeAidImg}
-          title='Refugee Aid'
-          framework='React / / Ruby on Rails'
-          belink={'https://github.com/Refugee-Aid-Capstone/refugee_aid_be'}
-          felink={'https://github.com/Refugee-Aid-Capstone/refugee-aid-fe'}
-          soundEnabled={soundEnabled} 
-          playSound={playSound} 
-          blipHoverSound={blipHoverSound} 
-          blipClickSound={blipClickSound}
-        />
-        <ProjectItem 
-          img={LabelLensImg}
-          title='Label Lens'
-          framework='Ruby on Rails'
-          belink={'https://github.com/Label-Lens/label_lens_be'}
-          felink={'https://github.com/Label-Lens/label_lens_fe'}
-          soundEnabled={soundEnabled} 
-          playSound={playSound} 
-          blipHoverSound={blipHoverSound} 
-          blipClickSound={blipClickSound}
-          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
