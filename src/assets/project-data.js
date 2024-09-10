c1016bac4b83875b6f9cb8dfc0f624b20ac4e162
Davis Weimer <davisweimer@gmail.com>
2024-09-10 10:04:56 -0600

    Add Project data to seperate file

---
+++ src/assets/project-data.js
@@ -0,0 +1,37 @@
+import RefugeeAidImg from '../assets/RefugeeAid.png';
+import WWVTImg from '../assets/wwvt.png';
+import WhetherSweater from '../assets/whether-sweater.png';
+import GenreFi from '../assets/genre-fi.png';
+
+const projectList = [
+  {
+    img: WhetherSweater,
+    title: 'Whether Sweater App',
+    framework: 'React + Vite / / Ruby on Rails',
+    belink: 'https://github.com/DavisWeimer/whether_sweater_api',
+    felink: 'https://github.com/DavisWeimer/whether-sweater-fe',
+    link: 'https://whether-sweater-fe.vercel.app/',
+  },
+  {
+    img: GenreFi,
+    title: 'Genre-Fi',
+    framework: 'React',
+    link: 'https://genre-fi.vercel.app/',
+    felink: 'https://github.com/DavisWeimer/genre-fi',
+  },
+  {
+    img: RefugeeAidImg,
+    title: 'Refugee Aid',
+    framework: 'React / / Ruby on Rails',
+    belink: 'https://github.com/Refugee-Aid-Capstone/refugee_aid_be',
+    felink: 'https://github.com/Refugee-Aid-Capstone/refugee-aid-fe',
+  },
+  {
+    img: WWVTImg,
+    title: "World's Wonder View Tower",
+    framework: 'SquareSpace',
+    link: 'https://www.friendsofthegenoatower.org/',
+  },
+];
+
+export default projectList;
