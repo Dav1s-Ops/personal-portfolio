58632b2e08cf9493c32f6d7d4a9d836d6ae43a52
Davis Weimer <davisweimer@gmail.com>
2024-09-11 09:37:56 -0600

    Update to projects

--- src/assets/project-data.js
+++ src/assets/project-data.js
@@ -1,4 +1,5 @@
 import RefugeeAidImg from '../assets/RefugeeAid.png';
+import AccoladerImg from '../assets/accolader.png'
 import WWVTImg from '../assets/wwvt.png';
 import WhetherSweater from '../assets/whether-sweater.png';
 import GenreFi from '../assets/genre-fi.png';
@@ -7,7 +8,7 @@ const projectList = [
   {
     img: WhetherSweater,
     title: 'Whether Sweater App',
-    framework: 'React + Vite / / Ruby on Rails',
+    framework: 'React / / Ruby on Rails',
     belink: 'https://github.com/Dav1s-Ops/whether_sweater_api',
     felink: 'https://github.com/Dav1s-Ops/whether-sweater-fe',
     link: 'https://whether-sweater-fe.vercel.app/',
@@ -20,11 +21,10 @@ const projectList = [
     felink: 'https://github.com/Dav1s-Ops/genre-fi',
   },
   {
-    img: RefugeeAidImg,
-    title: 'Refugee Aid',
-    framework: 'React / / Ruby on Rails',
-    belink: 'https://github.com/Refugee-Aid-Capstone/refugee_aid_be',
-    felink: 'https://github.com/Refugee-Aid-Capstone/refugee-aid-fe',
+    img: AccoladerImg,
+    title: 'Accolader.io',
+    framework: 'Ruby on Rails / / AWS // Docker',
+    link: 'https://accolader.io/'
   },
   {
     img: WWVTImg,
